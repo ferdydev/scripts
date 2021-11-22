a805cd37722687774c25c54e9b14142ed2f1a771
ferdinand <ferdynkot2@gmail.com>
2021-11-22 14:17:33 -0600

    my fun

---
+++ fun1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+services:
+    wordpress:
+       depends on:
+          -
+          -
+       image:
+       environment:
+          -
+          -
+          -
+       ports:
+       networks:
+          -webs
+       volumes:
+    msql:
+      image:
+      environment:
+          -
+          -
+      ports:
+      networks:
+          -webs
